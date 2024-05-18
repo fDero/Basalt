9445208a279ef9e3c35e4702165bffe8ee9171ca
fDero <francescodero@outlook.it>
2024-05-19 01:46:16 +0200

    Minor changes to 'Polymorph' (better names for template parameters)

--- include/misc/polymorph.hpp
+++ include/misc/polymorph.hpp
@@ -3,29 +3,29 @@
 #include <memory>
 #include <string>
 
-template<typename InnerStruct> 
+template<typename Interface> 
 class Polymorph {
     public: 
         
         Polymorph(const Polymorph& other)
             : ptr(other.ptr), cpp_type_info_str(other.cpp_type_info_str) { }
 
-        template<typename ConcreteInnerStruct> 
-        Polymorph(const ConcreteInnerStruct& type)
-            requires(std::is_base_of_v<InnerStruct, ConcreteInnerStruct>){ 
-                ptr = std::make_shared<ConcreteInnerStruct>(type); 
-                cpp_type_info_str = typeid(ConcreteInnerStruct).name();
+        template<typename Implementation> 
+        Polymorph(const Implementation& type)
+            requires(std::is_base_of_v<Interface, Implementation>){ 
+                ptr = std::make_shared<Implementation>(type); 
+                cpp_type_info_str = typeid(Implementation).name();
         }
 
-        template<typename ConcreteInnerStruct> 
+        template<typename Implementation> 
         bool is() const
-            requires(std::is_base_of_v<InnerStruct, ConcreteInnerStruct>){ 
-                return cpp_type_info_str == typeid(ConcreteInnerStruct).name(); 
+            requires(std::is_base_of_v<Interface, Implementation>){ 
+                return cpp_type_info_str == typeid(Implementation).name(); 
         }
         
-        template<typename ConcreteInnerStruct> 
-        const ConcreteInnerStruct& get() const
-            requires(std::is_base_of_v<InnerStruct, ConcreteInnerStruct>) { 
+        template<typename Implementation> 
+        const Implementation& get() const
+            requires(std::is_base_of_v<Interface, Implementation>) { 
                 
                 //this function it's in "errors/internal_errors.hpp" but
                 //include is impossible because it causes cyclic dependencies
@@ -35,19 +35,19 @@ class Polymorph {
                     const std::type_info& type_info
                 );
                 
-                assert_get_operation_is_possible(cpp_type_info_str, typeid(ConcreteInnerStruct)); 
-                return *static_cast<ConcreteInnerStruct*>(ptr.get());
+                assert_get_operation_is_possible(cpp_type_info_str, typeid(Implementation)); 
+                return *static_cast<Implementation*>(ptr.get());
         }
 
-        template<typename ConcreteInnerStruct> 
-        ConcreteInnerStruct& get()
-            requires(std::is_base_of_v<InnerStruct, ConcreteInnerStruct>) { 
-                return const_cast<ConcreteInnerStruct&>(
-                    static_cast<const Polymorph&>(*this).get<ConcreteInnerStruct>()
+        template<typename Implementation> 
+        Implementation& get()
+            requires(std::is_base_of_v<Interface, Implementation>) { 
+                return const_cast<Implementation&>(
+                    static_cast<const Polymorph&>(*this).get<Implementation>()
                 );
         }
 
     protected:
-        std::shared_ptr<InnerStruct> ptr;
+        std::shared_ptr<Interface> ptr;
         std::string cpp_type_info_str;
 };
