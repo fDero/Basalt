efaac476cb99d8090ca13a6cd60dd7eecca4b08b
fDero <francescodero@outlook.it>
2024-05-29 20:12:22 +0200

    Add some more test cases for 'AssignmentTypeChecker'

--- tests/preprocessor/assignment_type_checking_on_template_types.cpp
+++ tests/preprocessor/assignment_type_checking_on_template_types.cpp
@@ -116,9 +116,9 @@ TEST(Preprocessor, List_Of_Ints_And_List_Of_Number_Are_Compatible_With_List_Of_T
     TypeSignature list_of_numbers = CustomType { Token { "List", "main.basalt", 1, 1, 1, Token::Type::type }, { number_type } };
     TypeSignature list_of_Ts = CustomType { Token { "List", "main.basalt", 1, 1, 1, Token::Type::type }, { TypeSignatureFactory::T } };
     bool list_of_numbers_compatible_with_list_of_Ts = type_checker.validate_assignment(list_of_numbers, list_of_Ts);
-    bool int_is_compatible_with_template_t = type_checker.validate_assignment(list_of_ints, list_of_Ts);
+    bool list_of_ints_compatible_with_list_of_Ts = type_checker.validate_assignment(list_of_ints, list_of_Ts);
     EXPECT_TRUE(list_of_numbers_compatible_with_list_of_Ts);
-    EXPECT_TRUE(int_is_compatible_with_template_t);
+    EXPECT_TRUE(list_of_ints_compatible_with_list_of_Ts);
     EXPECT_EQ(type_checker.get_generic_substitution_rules().size(), 1);
     EXPECT_EQ(type_checker.get_generic_substitution_rules().back().to_be_substituded, "T");
     ASSERT_TRUE(type_checker.get_generic_substitution_rules().back().replacement.is<CustomType>());
@@ -126,8 +126,8 @@ TEST(Preprocessor, List_Of_Ints_And_List_Of_Number_Are_Compatible_With_List_Of_T
 }
 
 TEST(Preprocessor, List_Of_Ints_And_List_Of_Number_Are_Non_Mutually_Compatible_With_Each_Other) {
-    ProgramRepresentation simple_union_definition_program;
-    simple_union_definition_program.store_definitions_from_file(
+    ProgramRepresentation simple_multi_definition_program;
+    simple_multi_definition_program.store_definitions_from_file(
         Filerepresentation {
             .file_metadata = {
                 .filename = "main.basalt",
@@ -148,7 +148,7 @@ TEST(Preprocessor, List_Of_Ints_And_List_Of_Number_Are_Non_Mutually_Compatible_W
             .func_defs = { }
         }
     );
-    AssignmentTypeChecker type_checker(simple_union_definition_program);
+    AssignmentTypeChecker type_checker(simple_multi_definition_program);
     TypeSignature number_type = CustomType { Token { "Number", "main.basalt", 1, 1, 1, Token::Type::type }, {} };
     TypeSignature list_of_ints = CustomType { Token { "List", "main.basalt", 1, 1, 1, Token::Type::type }, { TypeSignatureFactory::Int } };
     TypeSignature list_of_numbers = CustomType { Token { "List", "main.basalt", 1, 1, 1, Token::Type::type }, { number_type } };
--- tests/preprocessor/assignment_type_checking_on_type_aliases.cpp
+++ tests/preprocessor/assignment_type_checking_on_type_aliases.cpp
@@ -156,3 +156,82 @@ TEST(Preprocessor, Pair_Of_Int_Is_Compatible_With_Pair_Of_T_U) {
     ASSERT_TRUE(type_checker.get_generic_substitution_rules().back().replacement.is<PrimitiveType>());
     EXPECT_EQ(type_checker.get_generic_substitution_rules().back().replacement.get<PrimitiveType>().type_name, "Int");
 }
+
+TEST(Preprocessor, List_Of_Aliased_Ints_And_List_Of_Number_Are_Compatible_With_List_Of_T) {
+    ProgramRepresentation simple_multi_definition_program;
+    simple_multi_definition_program.store_definitions_from_file(
+        Filerepresentation {
+            .file_metadata = {
+                .filename = "main.basalt",
+                .packagename = "testpackage",
+                .imports = { }
+            },
+            .type_defs = { 
+                StructDefinitionFactory::make_struct_definition(
+                    "List", { "T "}, { StructDefinitionFactory::no_fields }
+                ),
+                TypeAliasFactory::make_type_alias(
+                    "AliasedInt", { }, TypeSignatureFactory::Int
+                ),
+                UnionDefinitionFactory::make_union_definition(
+                    "Number", { }, {
+                        TypeSignatureFactory::Int,
+                        TypeSignatureFactory::Float
+                    }
+                )
+            },
+            .func_defs = { }
+        }
+    );
+    AssignmentTypeChecker type_checker(simple_multi_definition_program);
+    TypeSignature aliased_int = CustomType { Token { "AliasedInt", "main.basalt", 1, 1, 1, Token::Type::type }, {} };
+    TypeSignature number_type = CustomType { Token { "Number", "main.basalt", 1, 1, 1, Token::Type::type }, {} };
+    TypeSignature list_of_aliased_ints = CustomType { Token { "List", "main.basalt", 1, 1, 1, Token::Type::type }, { aliased_int } };
+    TypeSignature list_of_numbers = CustomType { Token { "List", "main.basalt", 1, 1, 1, Token::Type::type }, { number_type } };
+    TypeSignature list_of_Ts = CustomType { Token { "List", "main.basalt", 1, 1, 1, Token::Type::type }, { TypeSignatureFactory::T } };
+    bool list_of_numbers_compatible_with_list_of_Ts = type_checker.validate_assignment(list_of_numbers, list_of_Ts);
+    bool list_of_aliased_ints_compatible_with_list_of_Ts = type_checker.validate_assignment(list_of_aliased_ints, list_of_Ts);
+    EXPECT_TRUE(list_of_numbers_compatible_with_list_of_Ts);
+    EXPECT_TRUE(list_of_aliased_ints_compatible_with_list_of_Ts);
+    EXPECT_EQ(type_checker.get_generic_substitution_rules().size(), 1);
+    EXPECT_EQ(type_checker.get_generic_substitution_rules().back().to_be_substituded, "T");
+    ASSERT_TRUE(type_checker.get_generic_substitution_rules().back().replacement.is<CustomType>());
+    EXPECT_EQ(type_checker.get_generic_substitution_rules().back().replacement.get<CustomType>().type_name, "Number");
+}
+
+TEST(Preprocessor, List_Of_Aliased_Ints_And_List_Of_Number_Are_Non_Mutually_Compatible_With_Each_Other) {
+    ProgramRepresentation simple_multi_definition_program;
+    simple_multi_definition_program.store_definitions_from_file(
+        Filerepresentation {
+            .file_metadata = {
+                .filename = "main.basalt",
+                .packagename = "testpackage",
+                .imports = { }
+            },
+            .type_defs = { 
+                StructDefinitionFactory::make_struct_definition(
+                    "List", { "T" }, { StructDefinitionFactory::no_fields }
+                ),
+                TypeAliasFactory::make_type_alias(
+                    "AliasedInt", { }, TypeSignatureFactory::Int
+                ),
+                UnionDefinitionFactory::make_union_definition(
+                    "Number", { }, {
+                        TypeSignatureFactory::Int,
+                        TypeSignatureFactory::Float
+                    }
+                )
+            },
+            .func_defs = { }
+        }
+    );
+    AssignmentTypeChecker type_checker(simple_multi_definition_program);
+    TypeSignature aliased_int = CustomType { Token { "AliasedInt", "main.basalt", 1, 1, 1, Token::Type::type }, {} };
+    TypeSignature number_type = CustomType { Token { "Number", "main.basalt", 1, 1, 1, Token::Type::type }, {} };
+    TypeSignature list_of_aliased_ints = CustomType { Token { "List", "main.basalt", 1, 1, 1, Token::Type::type }, { aliased_int } };
+    TypeSignature list_of_numbers = CustomType { Token { "List", "main.basalt", 1, 1, 1, Token::Type::type }, { number_type } };
+    bool list_of_numbers_compatible_with_list_of_aliased_ints = type_checker.validate_assignment(list_of_numbers, list_of_aliased_ints);
+    bool list_of_aliased_ints_compatible_with_list_of_numbers = type_checker.validate_assignment(list_of_aliased_ints, list_of_numbers);
+    EXPECT_FALSE(list_of_numbers_compatible_with_list_of_aliased_ints);
+    EXPECT_FALSE(list_of_aliased_ints_compatible_with_list_of_numbers);
+}
