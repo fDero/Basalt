50e942846734e8260258c1f90eea11f0fe16ea87
fDero <francescodero@outlook.it>
2024-11-06 02:44:36 +0100

    Show correct LICENSE in .msi Windows installer

--- msi/MSIGenerator.cs
+++ msi/MSIGenerator.cs
@@ -8,11 +8,13 @@ namespace BasaltInstaller
         static void Main()
         {
             string exePath = @"..\out\bin\basalt.exe";
+            string licensePath = @"..\LICENSE.rtf";
             var project = new Project("basalt-v0.0.0-alpha",
                 new Dir(@"%ProgramFiles%\basalt\v0.0.0-alpha",
                     new File(exePath)
                 )
             );
+            project.LicenceFile = licensePath;
             project.GUID = new Guid("03d24f34-2adb-4bc9-8325-94a90aefdb11");
             project.BuildMsi();
         }
