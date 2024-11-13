8fd822fd2ba7d14debbc2db945e43bda103d34e8
fDero <francescodero@outlook.it>
2024-11-13 03:18:22 +0100

    Update named-unions syntax

--- src/parser/type_definitions.cpp
+++ src/parser/type_definitions.cpp
@@ -9,6 +9,8 @@
 #include "frontend/parser.hpp"
 #include "language/expressions.hpp"
 
+#include "errors/preprocessing_errors.hpp" // <-- ensure_typesignature_is
+
 [[nodiscard]] StructDefinition::Field Parser::parse_struct_field() {
     assert_token_is_text(source_tokens, iterator);
     std::string field_name = iterator->sourcetext;
@@ -41,12 +43,11 @@
     UnionDefinition union_def(*iterator);
     std::advance(iterator, 1);
     union_def.template_generics_names = parse_template_generics();
-    ensure_token_matches(source_tokens, iterator++, "{");
-    while (iterator != source_tokens.end() && iterator->sourcetext != "}") {
-        union_def.types.push_back(parse_typesignature());
-        ensure_token_matches(source_tokens, iterator++, ";");
-    }
-    ensure_token_matches(source_tokens, iterator++, "}");
+    ensure_token_matches(source_tokens, iterator++, "=");
+    TypeSignature inline_union_ts = parse_typesignature();
+    ensure_typesignature_is<InlineUnion>(inline_union_ts);
+    union_def.types = std::move(inline_union_ts.get<InlineUnion>().alternatives);
+    ensure_token_matches(source_tokens, iterator++, ";");
     return union_def;
 }
 
---
+++ tests/frontend/parser/union_definitions_with_generics.cpp
@@ -0,0 +1,26 @@
+
+#include <gtest/gtest.h>
+#include "frontend/parser.hpp"
+#include "errors/internal_errors.hpp"
+#include "errors/parsing_errors.hpp"
+
+TEST(Parsing, Union_Generic_Either_Definition) {
+    std::vector<Token> tokens = {
+        { "union",   "test.basalt",  1,  1,  1,   Token::Type::struct_keyword },
+        { "Either",  "test.basalt",  1,  2,  7,   Token::Type::type           },
+        { "<",       "test.basalt",  1,  3,  8,   Token::Type::symbol         },
+        { "T",       "test.basalt",  1,  2,  9,   Token::Type::type           },
+        { ",",       "test.basalt",  1,  2,  10,  Token::Type::symbol         },
+        { "U",       "test.basalt",  1,  2,  11,  Token::Type::type           },
+        { ">",       "test.basalt",  1,  4,  12,  Token::Type::symbol         },
+        { "=",       "test.basalt",  1,  3,  13,  Token::Type::symbol         },
+        { "T",       "test.basalt",  1,  4,  14,  Token::Type::type           },
+        { "|",       "test.basalt",  1,  3,  13,  Token::Type::symbol         },
+        { "U",       "test.basalt",  1,  4,  14,  Token::Type::type           },
+        { ";",       "test.basalt",  1,  3,  13,  Token::Type::symbol         }
+    };
+    Parser parser = Parser({ "inline-tests.basalt", tokens });
+    UnionDefinition union_def = parser.parse_union_definition();
+    EXPECT_EQ(union_def.types.size(), 2);
+    EXPECT_EQ(union_def.template_generics_names.size(), 2);
+}
